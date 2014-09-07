1b038752c00fc64670fb93d3b57d877d31f62d71
bzflebor <bzflebor@motodashi.local>
2014-09-07 15:14:49 +0200

    Fix indentation.

--- src/mzl350i.c
+++ src/mzl350i.c
@@ -42,241 +42,236 @@ int hsize=0, vsize=0;
 
 void wr_comm(uchar out_data)              
 {
-	LCD_CS_CLR;
-	LCD_RS_CLR;
-		
-	bcm2835_spi_transfer(out_data);
+    LCD_CS_CLR;
+    LCD_RS_CLR;
 
-	LCD_CS_SET;
+    bcm2835_spi_transfer(out_data);
+
+    LCD_CS_SET;
 }
 
 void wr_dat(uchar out_data)              
 {
-	LCD_CS_CLR;
-	LCD_RS_SET;	
+    LCD_CS_CLR;
+    LCD_RS_SET;
 
-	bcm2835_spi_transfer(out_data);
+    bcm2835_spi_transfer(out_data);
 
-	LCD_CS_SET;
+    LCD_CS_SET;
 }
 
 void LCD_WR_Data(uint val)
 {
-	bcm2835_spi_transfer(val>>8);
-	bcm2835_spi_transfer(val);
+    bcm2835_spi_transfer(val>>8);
+    bcm2835_spi_transfer(val);
 }
 
 uint LCD_RD_REG16(uint index)
 {
-	uint pd;
+    uint pd;
 
-	return pd;;
+    return pd;;
 }
-
 
 void LCD_Init()
 {
-	LCD_RST_CLR;
-	delay (100);
-	LCD_RST_SET;
-	delay (100);
+    LCD_RST_CLR;
+    delay (100);
+    LCD_RST_SET;
+    delay (100);
 
-  wr_comm(0x0011);
-  delay(1);
-  wr_comm(0x0013);
-  delay(1);
+    wr_comm(0x0011);
+    delay(1);
+    wr_comm(0x0013);
+    delay(1);
 
-  wr_comm(0x00C0);
-  wr_dat(0x0000);
-  wr_dat(0x003B);
-  wr_dat(0x0000);
-  wr_dat(0x0002);
-  wr_dat(0x0011);
+    wr_comm(0x00C0);
+    wr_dat(0x0000);
+    wr_dat(0x003B);
+    wr_dat(0x0000);
+    wr_dat(0x0002);
+    wr_dat(0x0011);
 
-  wr_comm(0x00C1);
-  wr_dat(0x0010);
-  wr_dat(0x0010);//0B);
-  wr_dat(0x0068);//88);
-  
-  wr_comm(0x00C5);
-  wr_dat(0x0001);
-  
-  wr_comm(0x00C8);
-  wr_dat(0x0000);
-  wr_dat(0x0030); //30
-  wr_dat(0x0036);
-  wr_dat(0x0045);
-  wr_dat(0x0004);
-  wr_dat(0x0016);
-  wr_dat(0x0037);
-  wr_dat(0x0075);
-  wr_dat(0x0077);
-  wr_dat(0x0054);
-  wr_dat(0x0006);//f);
-  wr_dat(0x0012);//00);
-   
-  wr_comm(0x00D0);
-  wr_dat(0x0007);
-  wr_dat(0x0040);//40); //41   
-  wr_dat(0x001f);//1c);//1e
+    wr_comm(0x00C1);
+    wr_dat(0x0010);
+    wr_dat(0x0010);//0B);
+    wr_dat(0x0068);//88);
+
+    wr_comm(0x00C5);
+    wr_dat(0x0001);
+
+    wr_comm(0x00C8);
+    wr_dat(0x0000);
+    wr_dat(0x0030); //30
+    wr_dat(0x0036);
+    wr_dat(0x0045);
+    wr_dat(0x0004);
+    wr_dat(0x0016);
+    wr_dat(0x0037);
+    wr_dat(0x0075);
+    wr_dat(0x0077);
+    wr_dat(0x0054);
+    wr_dat(0x0006);//f);
+    wr_dat(0x0012);//00);
+
+    wr_comm(0x00D0);
+    wr_dat(0x0007);
+    wr_dat(0x0040);//40); //41
+    wr_dat(0x001f);//1c);//1e
 
     
-  wr_comm(0x00D1);
-  wr_dat(0x0000);
-  wr_dat(0x0018);//18);
-  wr_dat(0x0006);//1d);
-  
-  wr_comm(0x00D2);
-  wr_dat(0x0001);
-  wr_dat(0x0011);//11
+    wr_comm(0x00D1);
+    wr_dat(0x0000);
+    wr_dat(0x0018);//18);
+    wr_dat(0x0006);//1d);
 
-  wr_comm(0x00E4);
-  wr_dat(0x00A0);
-  
+    wr_comm(0x00D2);
+    wr_dat(0x0001);
+    wr_dat(0x0011);//11
+
+    wr_comm(0x00E4);
+    wr_dat(0x00A0);
+
     wr_comm(0x00F0);
-  wr_dat(0x0001);
-  
-  wr_comm(0x00F3);
-  wr_dat(0x0040);
-  wr_dat(0x000A);
-  
-  wr_comm(0x00F7);
-  wr_dat(0x0080);
-  
-  wr_comm(0x0036);
-  wr_dat(0x0028);//02);
-  
-  wr_comm(0x003A);
-  wr_dat(0x0005);
-  
-wr_comm(0x00B3);
-wr_dat(0x0000);
-wr_dat(0x0000);
-wr_dat(0x0000);
-wr_dat(0x0011);
-  
-  wr_comm(0x002B);
-wr_dat(0x0000);
-wr_dat(0x0000);
-wr_dat(0x0001);
-wr_dat(0x003F);
+    wr_dat(0x0001);
 
-wr_comm(0x002A);
-wr_dat(0x0000);
-wr_dat(0x0000);
-wr_dat(0x0001);
-wr_dat(0x00df);
-  
-  delay(1);
-  wr_comm(0x0029);
-  wr_comm(0x002C);
+    wr_comm(0x00F3);
+    wr_dat(0x0040);
+    wr_dat(0x000A);
+
+    wr_comm(0x00F7);
+    wr_dat(0x0080);
+
+    wr_comm(0x0036);
+    wr_dat(0x0028);//02);
+
+    wr_comm(0x003A);
+    wr_dat(0x0005);
+
+    wr_comm(0x00B3);
+    wr_dat(0x0000);
+    wr_dat(0x0000);
+    wr_dat(0x0000);
+    wr_dat(0x0011);
+
+    wr_comm(0x002B);
+    wr_dat(0x0000);
+    wr_dat(0x0000);
+    wr_dat(0x0001);
+    wr_dat(0x003F);
+
+    wr_comm(0x002A);
+    wr_dat(0x0000);
+    wr_dat(0x0000);
+    wr_dat(0x0001);
+    wr_dat(0x00df);
+
+    delay(1);
+    wr_comm(0x0029);
+    wr_comm(0x002C);
 }
+
 void ili9481_SetCursor(uint x,uint y)
 {
-	wr_comm(0x002B);  
-	wr_dat(x>>8);
-	wr_dat(0x00FF&x);
-	wr_dat(0x0001);
-	wr_dat(0x003F);
+    wr_comm(0x002B);
+    wr_dat(x>>8);
+    wr_dat(0x00FF&x);
+    wr_dat(0x0001);
+    wr_dat(0x003F);
 
-	wr_comm(0x002A);
-	wr_dat(y>>8);
-	wr_dat(0x00FF&y);
-	wr_dat(0x0001);
-	wr_dat(0x00df);
+    wr_comm(0x002A);
+    wr_dat(y>>8);
+    wr_dat(0x00FF&y);
+    wr_dat(0x0001);
+    wr_dat(0x00df);
 }
+
 void ili9481_Setwindow(uint xs,uint xe,uint ys,uint ye)
 {
-	wr_comm(0x002a);  
-	wr_dat(xs>>8);
-	wr_dat(xs);
-	wr_dat(xe>>8);
-	wr_dat(xe);
+    wr_comm(0x002a);
+    wr_dat(xs>>8);
+    wr_dat(xs);
+    wr_dat(xe>>8);
+    wr_dat(xe);
 
-	wr_comm(0x002b);
-	wr_dat(ys>>8);
-	wr_dat(ys);
-	wr_dat(ye>>8);
-	wr_dat(ye);
-	wr_comm(0x002C);
+    wr_comm(0x002b);
+    wr_dat(ys>>8);
+    wr_dat(ys);
+    wr_dat(ye>>8);
+    wr_dat(ye);
+    wr_comm(0x002C);
 }
+
 void TFTSetXY(uint x,uint y)
 {
     ili9481_SetCursor(x,y);
-    wr_comm(0x002C); 
+    wr_comm(0x002C);
 }
 
 void LCD_test()
 {
-	uint temp,num,i;
-	char n;
-		
-	ili9481_Setwindow(0,480-1,0,320-1);
+    uint temp,num,i;
+    char n;
 
-	LCD_CS_CLR;
-	LCD_RS_SET;
+    ili9481_Setwindow(0,480-1,0,320-1);
 
-	for(n=0;n<8;n++)
-	{
-	    temp=color[n];
-		for(num=40*480;num>0;num--)
-		{
-			LCD_WR_Data(temp);
-		}
-	}
-	for(n=0;n<3;n++)
-	{
-		ili9481_Setwindow(0,480-1,0,320-1);
-		LCD_CS_CLR;
-		LCD_RS_SET;
+    LCD_CS_CLR;
+    LCD_RS_SET;
 
-	    temp=color[n];
-		for(i=0;i<320;i++)
-		{
-			for(num=0;num<480;num++)
-			{
-		  		LCD_WR_Data(temp);
-		  	}
-		}
-	}
-	LCD_CS_SET;
+    for (n=0;n<8;n++) {
+        temp=color[n];
+        for (num=40*480;num>0;num--) {
+            LCD_WR_Data(temp);
+        }
+    }
+    for (n=0;n<3;n++) {
+        ili9481_Setwindow(0,480-1,0,320-1);
+        LCD_CS_CLR;
+        LCD_RS_SET;
+
+        temp=color[n];
+        for (i=0;i<320;i++) {
+            for (num=0;num<480;num++) {
+                LCD_WR_Data(temp);
+            }
+        }
+    }
+    LCD_CS_SET;
 }
 
 void LCD_clear(uint p)
 {
-	uint i,j;
-	TFTSetXY(0,0);
-	LCD_CS_CLR;
-	LCD_RS_SET;	
+    uint i,j;
+    TFTSetXY(0,0);
+    LCD_CS_CLR;
+    LCD_RS_SET;
 
-	for(i=0;i<320;i++)
-	{
-		for(j=0;j<480;j++)
-		{
-		  LCD_WR_Data(p);
-		}
-	}
-	LCD_CS_SET;
+    for (i=0;i<320;i++) {
+        for (j=0;j<480;j++) {
+            LCD_WR_Data(p);
+        }
+    }
+    LCD_CS_SET;
 }
 
 void write_dot(char dx,int dy,int color)
 {
-  	wr_comm(0x002A);  
-	wr_dat(dy>>8);
-	wr_dat(dy);
-	//wr_dat(0x01);
-	//wr_dat(0xdF);
+    wr_comm(0x002A);
+    wr_dat(dy>>8);
+    wr_dat(dy);
+    //wr_dat(0x01);
+    //wr_dat(0xdF);
 
-	wr_comm(0x002B);
-	wr_dat(dx>>8);
-	wr_dat(dx);
-	//wr_dat(0x01);
-	//wr_dat(0x3F);
-	wr_comm(0x002C); 
- 	LCD_CS_CLR;
-	LCD_RS_SET;    
-	LCD_WR_Data(color);
- 	LCD_CS_SET;
+    wr_comm(0x002B);
+    wr_dat(dx>>8);
+    wr_dat(dx);
+    //wr_dat(0x01);
+    //wr_dat(0x3F);
+    wr_comm(0x002C);
+    LCD_CS_CLR;
+    LCD_RS_SET;
+    LCD_WR_Data(color);
+    LCD_CS_SET;
 }
 
 void loadFrameBuffer_diff_960640()
@@ -288,55 +283,51 @@ void loadFrameBuffer_diff_960640()
     int i,j,k,time=300;
     unsigned long offset=0;
     int p;
-	int r1,g1,b1;
-	int r,g,b;
-	long minsum=0;
-	long nowsum=0;
+    int r1,g1,b1;
+    int r,g,b;
+    long minsum=0;
+    long nowsum=0;
     int flag;
-	int ra,ga,ba;
+    int ra,ga,ba;
     int drawmap[2][ysize/2][xsize/2];
     int diffmap[ysize/2][xsize/2];
     int diffsx, diffsy, diffex, diffey;
     int numdiff=0;
     int area;
     
-	buffer = (unsigned char *) malloc(xsize * ysize * 2);
+    buffer = (unsigned char *) malloc(xsize * ysize * 2);
     fseek(infile, 0, 0);
     
-	if (fread (buffer, xsize * ysize *2, sizeof(unsigned char), infile) != 1)
-	{
-
-       	printf ("Read < %d chars when loading file %s\n", hsize*vsize*3, "ss");
-		printf ("config.txt setting error\n") ;
-		return;
-	}    
-	ili9481_Setwindow(0,480-1,0,320-1);
-	LCD_CS_CLR;
-	LCD_RS_SET;
+    if (fread (buffer, xsize * ysize *2, sizeof(unsigned char), infile) != 1) {
+        printf ("Read < %d chars when loading file %s\n", hsize*vsize*3, "ss");
+        printf ("config.txt setting error\n") ;
+        return;
+    }
+    ili9481_Setwindow(0,480-1,0,320-1);
+    LCD_CS_CLR;
+    LCD_RS_SET;
     
-	for (i=0; i < ysize/2; i++) 
-	{
-		for(j=0; j< xsize/2; j++) 
-		{
-			diffmap[i][j]=1;
-			drawmap[0][i][j]=0;
-			LCD_WR_Data(0);
-			drawmap[1][i][j]=255;
-		}
-	}
+    for (i=0; i < ysize/2; i++) {
+        for (j=0; j< xsize/2; j++) {
+            diffmap[i][j]=1;
+            drawmap[0][i][j]=0;
+            LCD_WR_Data(0);
+            drawmap[1][i][j]=255;
+        }
+    }
 
     flag=1;
     
-	while (1) {
-	//while (time--) {
+    while (1) {
+        //while (time--) {
         
         numdiff=0;
         flag=1-flag;
         diffex=diffey=0;
         diffsx=diffsy=65535;
 
-        for(i=0; i < ysize; i+=2){
-            for(j=0; j < xsize; j+=2) {
+        for (i=0; i < ysize; i+=2) {
+            for (j=0; j < xsize; j+=2) {
                 offset =  (i * xsize+ j)*2;
                 p=(buffer[offset+1] << 8) | buffer[offset];
                 r = (p & RGB565_MASK_RED) >> 11;
@@ -399,17 +390,17 @@ void loadFrameBuffer_diff_960640()
             }
             
         }
-        if (numdiff > 10){
-           // printf ("(%d, %d) - (%d, %d)\n",diffsx, diffsy, diffex, diffey);
+        if (numdiff > 10) {
+            // printf ("(%d, %d) - (%d, %d)\n",diffsx, diffsy, diffex, diffey);
             
             //area = ((abs(diffex - diffsx)+1)*(1+abs(diffey-diffsy)));
             //printf("diff:%d, area:%d, cov:%f\n",numdiff, area,(1.0*numdiff)/area);
         }
-        if (numdiff< 2){
-		ili9481_Setwindow(0,480-1,0,320-1);
-		LCD_CS_CLR;
-		LCD_RS_SET;
-            for (i=diffsx; i<=diffex; i++){
+        if (numdiff< 2) {
+            ili9481_Setwindow(0,480-1,0,320-1);
+            LCD_CS_CLR;
+            LCD_RS_SET;
+            for (i=diffsx; i<=diffex; i++) {
                 for (j=diffsy;j<=diffey; j++) {
                     if (diffmap[i][j]!=0)
                         write_dot(i,j,drawmap[flag][i][j]);
@@ -417,12 +408,12 @@ void loadFrameBuffer_diff_960640()
             }
             usleep(700L);
             
-        } else{
-		ili9481_Setwindow(diffsy,diffey,diffsx,diffex);
-		LCD_CS_CLR;
-		LCD_RS_SET;
+        } else {
+            ili9481_Setwindow(diffsy,diffey,diffsx,diffex);
+            LCD_CS_CLR;
+            LCD_RS_SET;
             //printf ("(%d, %d) - (%d, %d)\n",diffsx, diffsy, diffex, diffey);
-            for (i=diffsx; i<=diffex; i++){
+            for (i=diffsx; i<=diffex; i++) {
                 for (j=diffsy;j<=diffey; j++) {
                     LCD_WR_Data(drawmap[flag][i][j]);
                 }
@@ -461,19 +452,17 @@ void loadFrameBuffer_diff_480320()
     buffer = (unsigned char *) malloc(xsize * ysize * 2);
     fseek(infile, 0, 0);
     
-	if (fread (buffer, xsize * ysize *2, sizeof(unsigned char), infile) != 1)
-	{
+    if (fread (buffer, xsize * ysize *2, sizeof(unsigned char), infile) != 1) {
+        printf ("Read < %d chars when loading file %s\n", hsize*vsize*3, "ss");
+        printf ("config.txt setting error\n") ;
+        return;
+    }
+    ili9481_Setwindow(0,480-1,0,320-1);
+    LCD_CS_CLR;
+    LCD_RS_SET;
 
-       	printf ("Read < %d chars when loading file %s\n", hsize*vsize*3, "ss");
-		printf ("config.txt setting error\n") ;
-		return;
-	} 
-	ili9481_Setwindow(0,480-1,0,320-1);
-	LCD_CS_CLR;
-	LCD_RS_SET;
-  
     for (i=0; i < ysize; i++) {
-        for(j=0; j< xsize; j++) {
+        for (j=0; j< xsize; j++) {
             diffmap[i][j]=1;
             drawmap[0][i][j]=0;
             LCD_WR_Data(0);
@@ -490,9 +479,9 @@ void loadFrameBuffer_diff_480320()
         diffex=diffey=0;
         diffsx=diffsy=65535;
         
-        for(i=0; i < ysize; i++){
+        for (i=0; i < ysize; i++) {
             for(j=0; j < xsize; j++) {
-               offset =  (i * xsize+ j)*2;
+                offset =  (i * xsize+ j)*2;
                 p=(buffer[offset+1] << 8) | buffer[offset];
                 
                 //drawmap[flag][i>>1][j>>1] = p;
@@ -513,21 +502,21 @@ void loadFrameBuffer_diff_480320()
                 } else {
                     diffmap[i][j]=0;
                 }
-               // offset++;
+                // offset++;
             }
             
         }
-        if (numdiff > 400){
+        if (numdiff > 400) {
             // printf ("(%d, %d) - (%d, %d)\n",diffsx, diffsy, diffex, diffey);
             
             //area = ((abs(diffex - diffsx)+1)*(1+abs(diffey-diffsy)));
             //printf("diff:%d, area:%d, cov:%f\n",numdiff, area,(1.0*numdiff)/area);
         }
-        if (numdiff< 2){
-		ili9481_Setwindow(0,480-1,0,320-1);
-		LCD_CS_CLR;
-		LCD_RS_SET;
-            for (i=diffsx; i<=diffex; i++){
+        if (numdiff< 2) {
+            ili9481_Setwindow(0,480-1,0,320-1);
+            LCD_CS_CLR;
+            LCD_RS_SET;
+            for (i=diffsx; i<=diffex; i++) {
                 for (j=diffsy;j<=diffey; j++) {
                     if (diffmap[i][j]!=0)
                         write_dot(i,j,drawmap[flag][i][j]);
@@ -535,13 +524,13 @@ void loadFrameBuffer_diff_480320()
             }
             usleep(700L);
             
-        } else{
-		ili9481_Setwindow(diffsy,diffey,diffsx,diffex);
-		LCD_CS_CLR;
-		LCD_RS_SET;
+        } else {
+            ili9481_Setwindow(diffsy,diffey,diffsx,diffex);
+            LCD_CS_CLR;
+            LCD_RS_SET;
             
             //printf ("(%d, %d) - (%d, %d)\n",diffsx, diffsy, diffex, diffey);
-            for (i=diffsx; i<=diffex; i++){
+            for (i=diffsx; i<=diffex; i++) {
                 for (j=diffsy;j<=diffey; j++) {
                     LCD_WR_Data(drawmap[flag][i][j]);
                 }
@@ -554,11 +543,11 @@ void loadFrameBuffer_diff_480320()
             printf ("Read < %d chars when loading file %s\n", hsize*vsize*3, "ss");
     }
 }
+
 int main (void)
 {
     printf("bcm2835 init now\n");
-    if (!bcm2835_init())
-    {
+    if (!bcm2835_init()) {
         printf("bcm2835 init error\n");
         return 1;
     }
